3f4b77d89f333c598189e85346fabec63a233493
espeditojunior943 <00001110376017sp@al.educacao.sp.gov.br>
2024-12-04 07:24:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 64?',
+    'A raiz quadrada de 64 é 8.'
+  );
+  
+  criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca.'
+  );
+  
+  criaCartao(
+    'Ciência',
+    'O que é a fotossíntese?',
+    'A fotossíntese é o processo em que as plantas convertem luz solar em energia.'
+  );
+  
+  criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Pacífico.'
+  );
+  
+  criaCartao(
+    'Linguística',
+    'Qual é a língua mais falada no mundo?',
+    'A língua mais falada no mundo é o Mandarim.'
+  );
+  
